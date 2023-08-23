56bfb66c7fc184e14091065593e4dff2b8de44cd
Paul Ryan <paul.walker.ryan@gmail.com>
2023-08-22 22:29:48 -0600

    adding winners and losers

--- script.js
+++ script.js
@@ -1,110 +1,131 @@
 let x = "It's X's turn";
 let o = "It's O's turn";
 let turnText = $('#turn');
+$('#startBtn').on("click",startGame)
+$('#clearBtn').on("click", resetGame)
 
 function startGame() {
-
  turnText.text(x);
- $('#startBtn').removeEventListener("click", startGame())
+ $('#startBtn').off()
+ $("td").on("click", (event) => onSquareClick("#" + event.target.id))
 }
 
-
-$('#1').on("click",function(){
+function onSquareClick(idOfSquare){
     if (turnText.text() === x){
-        $('#1').text('X').addClass('table-danger');
+        $(idOfSquare).text('X').addClass('table-danger');
         turnText.text(o);
     } else {
-        $('#1').text('O').addClass('table-primary');
+        $(idOfSquare).text('O').addClass('table-primary');
         turnText.text(x);
     }
-   
+   $(idOfSquare).off()
+   checkForWinner()
 }
-)
 
-$('#2').on("click",function(){
-    if (turnText.text() === x){
-        $('#2').text('X').addClass('table-danger');
-        turnText.text(o);
-    } else {
-        $('#2').text('O').addClass('table-primary');;
-        turnText.text(x);
-    }
-}
-)
+function resetGame() {
+$("td").text("").removeClass('table-danger table-primary');
 
-$('#3').on("click",function(){
-    if (turnText.text() === x){
-        $('#3').text('X').addClass('table-danger');
-        turnText.text(o);
-    } else {
-        $('#3').text('O').addClass('table-primary');;
-        turnText.text(x);
-    }
+$('#startBtn').on("click",startGame)
+turnText.text('Ready to play?')
 }
-)
 
-$('#4').on("click",function(){
-    if (turnText.text() === x){
-        $('#4').text('X').addClass('table-danger');
-        turnText.text(o);
-    } else {
-        $('#4').text('O').addClass('table-primary');;
-        turnText.text(x);
+function checkForWinner() {
+    if ($('#1').text() =='X' && $('#2').text() == 'X' && $('#3').text() == 'X') {
+        turnText.text("X is the Winner!")
+        $("td").off()
+    } else if ($('#1').text() =='O' && $('#2').text() == 'O' && $('#3').text() == 'O') {
+        turnText.text("O is the Winner!")
+        $("td").off()
+    } else if ($('#4').text() =='X' && $('#5').text() == 'X' && $('#6').text() == 'X') {
+        turnText.text("X is the Winner!")
+        $("td").off()
+    } else if ($('#4').text() =='O' && $('#5').text() == 'O' && $('#6').text() == 'O') {
+        turnText.text("X is the Winner!")
+        $("td").off()
     }
 }
-)
 
-$('#5').on("click",function(){
-    if (turnText.text() === x){
-        $('#5').text('X').addClass('table-danger');
-        turnText.text(o);
-    } else {
-        $('#5').text('O').addClass('table-primary');;
-        turnText.text(x);
-    }
-}
-)
+// $('#1').on("click", () => onSquareClick('#1')
+// )
 
-$('#6').on("click",function(){
-    if (turnText.text() === x){
-        $('#6').text('X').addClass('table-danger');
-        turnText.text(o);
-    } else {
-        $('#6').text('O').addClass('table-primary');;
-        turnText.text(x);
-    }
-}
-)
+// $('#2').on("click", () => onSquareClick('#2')
+// )
 
-$('#7').on("click",function(){
-    if (turnText.text() === x){
-        $('#7').text('X').addClass('table-danger');
-        turnText.text(o);
-    } else {
-        $('#7').text('O').addClass('table-primary');;
-        turnText.text(x);
-    }
-}
-)
+// $('#3').on("click",function(){
+//     if (turnText.text() === x){
+//         $('#3').text('X').addClass('table-danger');
+//         turnText.text(o);
+//     } else {
+//         $('#3').text('O').addClass('table-primary');;
+//         turnText.text(x);
+//     }
+// }
+// )
 
-$('#8').on("click",function(){
-    if (turnText.text() === x){
-        $('#8').text('X').addClass('table-danger');
-        turnText.text(o);
-    } else {
-        $('#8').text('O').addClass('table-primary');;
-        turnText.text(x);
-    }
-}
-)
+// $('#4').on("click",function(){
+//     if (turnText.text() === x){
+//         $('#4').text('X').addClass('table-danger');
+//         turnText.text(o);
+//     } else {
+//         $('#4').text('O').addClass('table-primary');;
+//         turnText.text(x);
+//     }
+// }
+// )
 
-$('#9').on("click",function(){
-    if (turnText.text() === x){
-        $('#9').text('X').addClass('table-danger');
-        turnText.text(o);
-    } else {
-        $('#9').text('O').addClass('table-primary');;
-        turnText.text(x);
-    }
-}
-)
+// $('#5').on("click",function(){
+//     if (turnText.text() === x){
+//         $('#5').text('X').addClass('table-danger');
+//         turnText.text(o);
+//     } else {
+//         $('#5').text('O').addClass('table-primary');;
+//         turnText.text(x);
+//     }
+// }
+// )
+
+// $('#6').on("click",function(){
+//     if (turnText.text() === x){
+//         $('#6').text('X').addClass('table-danger');
+//         turnText.text(o);
+//     } else {
+//         $('#6').text('O').addClass('table-primary');;
+//         turnText.text(x);
+//     }
+// }
+// )
+
+// $('#7').on("click",function(){
+//     if (turnText.text() === x){
+//         $('#7').text('X').addClass('table-danger');
+//         turnText.text(o);
+//     } else {
+//         $('#7').text('O').addClass('table-primary');;
+//         turnText.text(x);
+//     }
+// }
+// )
+
+// $('#8').on("click",function(){
+//     if (turnText.text() === x){
+//         $('#8').text('X').addClass('table-danger');
+//         turnText.text(o);
+//     } else {
+//         $('#8').text('O').addClass('table-primary');;
+//         turnText.text(x);
+//     }
+// }
+// )
+
+// $('#9').on("click",function(){
+//     if (turnText.text() === x){
+//         $('#9').text('X').addClass('table-danger');
+//         turnText.text(o);
+//     } else {
+//         $('#9').text('O').addClass('table-primary');;
+//         turnText.text(x);
+//     }
+// }
+// )
+
+//
